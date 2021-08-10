94ee99db8623edfb3d8d0b519bd8ea2c1b94aad9
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2021-08-09 21:24:13 -0300

    feat: created axios files

---
+++ src/api/yelp.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: 'Bearer eczyKjLjGJKnDkDP3HmzhYM-S3jT4q_6giU1HmrOMaGje7pj98o8otLBBnTHhHKBkQgm26G2XsaDBZ0Lx7R-ndAD60BafCME8r-tfZ7Pc672_GR5cWQ1HHSjaQsRYXYx'
+    }
+});
